f99a6f3544710ab8d0ef6fc7b7b5e3fcb5fce031
magic-pie <36836440+magic-pie@users.noreply.github.com>
2023-10-22 13:22:41 +0700

    fix an error

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -9,10 +9,10 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) readIDParam(r *http.Request) (int, error) {
+func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
-	id, err := strconv.Atoi(params.ByName("id"))
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 	if err != nil || id < 1 {
 		return 0, errors.New("invalid id parameter")
 	}
