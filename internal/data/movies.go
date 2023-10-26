818d204137b4cfea18a55642a6186c93175578f2
magic-pie <36836440+magic-pie@users.noreply.github.com>
2023-10-26 22:07:51 +0700

    9.6. Sorting lists

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/lib/pq"
@@ -134,13 +135,13 @@ func (m MovieModel) Delete(id int64) error {
 }
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := `
+	query := fmt.Sprintf(`
 		select id, created_at, title, year, runtime, genres, version
 		from movies
 		where (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) or $1 = '')
 		and (genres @> $2 or $2 = '{}')
-		order by id
-	`
+		order by %s %s, id ASC
+	`, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
